a7e336a843945f66c1d5d3fe85b02604abeb05b4
plasticcc <51684390+pdflu@users.noreply.github.com>
2021-10-07 16:00:08 +0930

    321321321

---
+++ CUSHIONS/.ipynb_checkpoints/3.9-checkpoint.py
@@ -0,0 +1,18 @@
+from qiskit import QuantumCircuit, assemble, Aer
+from math import pi, sqrt
+from qiskit.visualization import plot_bloch_multivector, plot_histogram
+sim = Aer.get_backend('aer_simulator')
+
+# Create the X-measurement function:
+def x_measurement(qc, qubit, cbit):
+    """Measure 'qubit' in the X-basis, and store the result in 'cbit'"""
+    qc.h(qubit)
+    qc.measure(qubit, cbit)
+    return qc
+
+initial_state = [1/sqrt(2), -1/sqrt(2)]
+# Initialize our qubit and measure it
+qc = QuantumCircuit(1,1)
+qc.initialize(initial_state, 0)
+x_measurement(qc, 0, 0)  # measure qubit 0 to classical bit 0
+qc.draw()
